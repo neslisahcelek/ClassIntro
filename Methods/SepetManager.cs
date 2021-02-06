1b3347a6268dd756e2dc10ab3157330bbe79332a
Neslişah Çelek <neslisah.celek@outlook.com>
2021-02-06 14:55:47 +0300

    16 Jan Değişiklikler yapıldı

---
+++ Methods/SepetManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methods
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine(urun.Adi + " Sepetinize Eklendi");
+        }
+    }
+}
