5b516d8a21e555e7b107c22c9392e440b550a6d0
Neslişah Çelek <neslisah.celek@outlook.com>
2021-02-14 13:34:11 +0300

    14.02

---
+++ Constructors/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer { Id = 1, FirstName = "Neslişah", LastName = "Çelek", City = "Antalya" };
+            Customer customer2 = new Customer (2, "Sinan", "Çelek", "Kocaeli");
+
+        }
+    }
+
+    class Customer
+    {
+        //default constructor
+        public Customer()
+        {
+
+        }
+
+        public Customer(int id, string firstName, string lastName, string city )
+        {
+            id = Id;
+            firstName = FirstName;
+            lastName = LastName;
+            city = City;
+      
+        }
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ SQLQuery2.sql
@@ -0,0 +1,37 @@
+﻿Select ContactName Adi, CompanyName SirketAdi, City Sehir from Customers  
+       --customerstaki kolonların ismini kendin belirlemek istersen
+
+sElect * from Customers where City = 'London' -- * = tüm kolonlar
+--case insensitive
+select * from Products where CategoryID=1 or CategoryID=3
+
+select * from Products where CategoryID=1 and UnitPrice>=10
+
+select * from Products order by  CategoryID,ProductName
+
+select * from Products where CategoryID=1 order by  UnitPrice asc --ascending:artan-->default. desc: azalan
+
+select count(*) Adet from Products where CategoryID=2 --product sayısı --adet: kolonun ismi
+
+
+select categoryID, count(*) Adet from Products group by CategoryID having count(*)<10
+
+select categoryID, count(*) Adet from Products where UnitPrice>20 group by CategoryID having count(*)<10
+
+
+select * from Products inner join Categories on Products.CategoryID = Categories.CategoryID
+--DTO-> Data transformation object
+select Products.ProductID, Products.ProductName, Products.UnitPrice, Categories.CategoryName from Products inner join Categories on Products.CategoryID = Categories.CategoryID
+
+select * from Products p left join [Order Details] od on p.ProductID = od.ProductID
+--left:productsta var orderda yok  --boşluk olduğu için parantez
+
+select * from Customers c left join Orders o on c.CustomerID = o.CustomerID where o.CustomerID is null
+--hiç sipariş vermeyen müşterileri bulur
+
+
+
+
+
+
+
